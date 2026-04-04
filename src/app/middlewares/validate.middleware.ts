3778a3a5fff7fec310fe75c167daedaba300830e
Mark Andrey <markandreydelacruz@ymail.com>
2026-04-04 10:46:39 +0800

    implement validation middleware

---
+++ src/app/middlewares/validate.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+export const validate =
+  <T>(schema: z.ZodType<T>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation error",
+        errors: result.error.flatten(),
+      });
+    }
+
+    req.body = result.data;
+    next();
+  };
--- src/features/auth/auth.route.ts
+++ src/features/auth/auth.route.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
-
-import { login } from "@/features/auth/auth.controller.js";
+import { Router } from 'express';
+import { login } from '@/features/auth/auth.controller.js';
+import { validate } from "@/app/middlewares/validate.middleware.js";
+import { loginSchema } from "./auth.schema.js";
 
 const router = Router();
-
-router.post("/login", login);
+router.post('/login', validate(loginSchema), login);
 
 export default router;
