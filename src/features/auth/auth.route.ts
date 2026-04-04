f01349fb6ccd8456b149fe46a28649b32da8593a
Mark Andrey <markandreydelacruz@ymail.com>
2026-04-04 09:05:08 +0800

    use aliases for typescript6

--- src/features/auth/auth.route.ts
+++ src/features/auth/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { login } from "@/features/auth/auth.controller";
+import { login } from "@/features/auth/auth.controller.js";
 
 const router = Router();
 
