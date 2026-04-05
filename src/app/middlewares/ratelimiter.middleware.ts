6757c3dc530d0b53d6e6c45d657cbbec76042334
Mark Andrey <markandreydelacruz@ymail.com>
2026-04-05 10:07:51 +0800

    update rate limiter configuration for general and authentication routes

--- src/app/middlewares/ratelimiter.middleware.ts
+++ src/app/middlewares/ratelimiter.middleware.ts
@@ -1,13 +1,13 @@
 import rateLimit from "express-rate-limit";
 
-// Rate limiter for general API routes
+// Rate limiter for general API routes - 15 minutes, 1000 requests
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 1000,
 });
 
-// Rate limiter for authentication routes
+// Rate limiter for authentication routes - 10 minutes, 1000 requests
 export const authLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 10,
+  max: 1000,
 });
